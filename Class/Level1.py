4f4b42d499a2e8c97edc76480ecc5e55da1f8342
barshakoirala123 <varshakoirala@gmail.com>
2025-12-16 13:38:38 +0545

    Nothing

---
+++ Class/Level1.py
@@ -0,0 +1,19 @@
+#Class and Object
+class lover: 
+    name = "Aswin"
+    age = 23
+    face = "cute and hot"
+
+name = "aalu"
+
+print(lover.age)
+print(lover.name)
+print(name)
+
+class hubby():
+    height = 6
+
+print(hubby().height)
+
+
+
