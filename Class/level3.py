1a5c6951270b9d4323e75f4b658e86b5307613aa
barshakoirala123 <varshakoirala@gmail.com>
2025-12-21 15:37:06 +0545

    class level 3

--- Class/level3.py
+++ Class/level3.py
@@ -16,3 +16,20 @@ print("Barsha's subject:", Barsha.subject)  # Microbiology
 print("Aashi's eyes:", Aashi.eyes)          # black (inherited)
 print("Aashi's hair:", Aashi.hair)          # black (inherited)
 print("Aashi's subject:", Aashi.subject)    # Computer Science (child-specific)
+
+
+class Aswin:
+    Height = "6 feet"
+    Face = "cute"
+    Nose = "Sharp"
+    Skin = "Fair"
+
+class Ashraya(Aswin):
+    Hair = "Golden brown"
+
+print("Aswin's Face:", Ashraya.Face)
+print("Aswin's Height:", Ashraya.Height)
+print("Aswin's Nose:", Ashraya.Nose)
+
+print("Ashraya's Nose:", Ashraya.Nose)
+print("Ashraya's Hair:",Ashraya.Hair)
